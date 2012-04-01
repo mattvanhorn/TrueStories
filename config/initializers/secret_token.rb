d8096fe48a9d62d6ea142b2d6ae3ffaef447c6d7
Matt Van Horn <mattvh@scoopst.com>
2012-03-31 19:47:09 -0700

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrueStories::Application.config.secret_token = '25866aa9eea18cd6def3f1d569dcf993685ef898c4bb06112c7fb6c553b12a5c0ec415bc206ab6c8e055fe7498bc41d732a74f858e928ba69bc523c46cbb2629'
