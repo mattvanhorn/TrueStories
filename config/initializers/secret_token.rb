fc03d77fbef003a4cf993e1f157e813d189c9b47
Matt Van Horn <mattvh@scoopst.com>
2012-04-05 16:39:09 -0700

    enable heroku config var task

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TrueStories::Application.config.secret_token = '25866aa9eea18cd6def3f1d569dcf993685ef898c4bb06112c7fb6c553b12a5c0ec415bc206ab6c8e055fe7498bc41d732a74f858e928ba69bc523c46cbb2629'
+TrueStories::Application.config.secret_token = ENV['SECRET_TOKEN']
