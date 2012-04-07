4301cc05a3220c9a8a7451e91b9aa740c884f306
Matt Van Horn <mattvh@scoopst.com>
2012-04-07 09:59:25 -0700

    bug fixes

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -10,11 +10,11 @@ class Authentication < ActiveRecord::Base
       user = User.new do |u|
         case auth['provider']
         when 'twitter'
-          u.nickname = auth['info']['screen_name']
-          u.authentications.build(:provider => auth['provider'], :uid => auth['uid'])
+          u.nickname = auth['info']['nickname'] || auth['info']['name']
         when 'github'
           raise auth.inspect
         end
+        u.authentications.build(:provider => "#{auth['provider']}", :uid => "#{auth['uid']}")
       end
       return user if user.save
     end
