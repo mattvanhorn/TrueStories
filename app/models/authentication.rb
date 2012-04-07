97606cc4a904ed98ba9d37b7fe4d74afc1a401d1
Matt Van Horn <mattvh@scoopst.com>
2012-04-07 14:04:05 -0700

    assume a unified auth hash schema for authentication providers

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -3,18 +3,14 @@ class Authentication < ActiveRecord::Base
   belongs_to :user
 
   def self.get_user_from_omniauth(auth)
-    authentication = find_by_provider_and_uid(auth['provider'], auth['uid'])
+    provider, uid = auth['provider'].to_s, auth['uid'].to_s
+    authentication = find_by_provider_and_uid(provider, uid)
     if authentication
       return authentication.user if authentication
     else
       user = User.new do |u|
-        case auth['provider']
-        when 'twitter'
-          u.nickname = auth['info']['nickname'] || auth['info']['name']
-        when 'github'
-          raise auth.inspect
-        end
-        u.authentications.build(:provider => "#{auth['provider']}", :uid => "#{auth['uid']}")
+        u.nickname = auth['info']['name']
+        u.authentications.build(:provider => provider, :uid => uid)
       end
       return user if user.save
     end
